f88c09ad880882e46803a35556ccd26d7d7115c6
kimbora <ITWILL@DESKTOP-7JIVS4K>
2024-12-19 16:10:47 +0900

    기초설정

---
+++ Good_Buy/src/main/java/com/itwillbs/goodbuy/service/MemberService.java
@@ -0,0 +1,104 @@
+package com.itwillbs.goodbuy.service;
+
+import java.util.Map;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.itwillbs.goodbuy.mapper.MemberMapper;
+import com.itwillbs.goodbuy.vo.MailAuthInfo;
+import com.itwillbs.goodbuy.vo.MemberVO;
+
+@Service
+public class MemberService {
+	@Autowired
+	private MemberMapper mapper;
+
+	public int registMember(MemberVO member) {
+		return mapper.insertMember(member);
+	}
+
+
+
+	public void registMemberAuthInfo(MailAuthInfo mailauthInfo) {
+		MailAuthInfo dbMailAuthInfo = mapper.selectMailAuthInfo(mailauthInfo);
+		if (dbMailAuthInfo == null) {
+			mapper.insertMailAuthInfo(mailauthInfo);
+		} else {
+			mapper.updateMailAuthInfo(mailauthInfo);
+		}
+
+	}
+
+	public boolean requestEmailAuth(MailAuthInfo mailAuthInfo) {
+		boolean isAuthsuccess = false;
+
+
+		MailAuthInfo dbMailAuthInfo = mapper.selectMailAuthInfo(mailAuthInfo);
+		System.out.println("조회된 인증 정보" + dbMailAuthInfo);
+
+		// 인증정보 조회결과 판별
+		if (dbMailAuthInfo != null) {
+			if (mailAuthInfo.getAuth_code().equals(dbMailAuthInfo.getAuth_code())) { // 인증코드가 일치
+				mapper.updateMailAuthStatus(mailAuthInfo); //인증상태 업데이트
+				mapper.deleteMailAuthInfo(mailAuthInfo);
+
+				isAuthsuccess = true;
+			}
+		}
+
+		return isAuthsuccess;
+	}
+
+	public String getMemberPasswd(String id) {
+		return mapper.selectMemberPasswd(id);
+	}
+
+//	public int modifyMember(Map<String, String> map) {
+//		// TODO Auto-generated method stub
+//		return mapper.updateMember(map,mem_id);
+//	}
+//}
+
+	public MemberVO getMember(MemberVO member) {
+		return mapper.selectMember(member);
+		
+	}
+	
+	public MemberVO getMemberNick(MemberVO member) {
+		return mapper.selectMemberNick(member);
+		
+	}
+
+	public MemberVO getMemberMail(MemberVO member) {
+		return mapper.selectEmailId(member);
+	}
+
+
+	public int modifyMember(Map<String, String> map) {
+		System.out.println("@@@mem_passwd: " + map.get("mem_passwd"));
+		System.out.println("@@@mem_name: " + map.get("mem_name"));
+
+		return mapper.updateMember(map);
+	}
+
+	
+	public MemberVO getMemberEmail(String mem_email) {
+		return mapper.selectEmail(mem_email);
+	}
+
+	//회원상태 탈퇴 요청
+	public int withdrawMember(String id) {
+		return mapper.updateMemberStatus(id,3); //3 : 탈퇴
+	}
+
+	//비밀번호 찾기로 비밀번호 업데이트
+	public int setTempPasswd(String heshePasswd, String mem_email) {
+		return mapper.updateTempPasswd(heshePasswd,mem_email);
+	}
+
+
+
+
+
+}
---
+++ Good_Buy/src/main/java/com/itwillbs/goodbuy/vo/MemberVO.java
@@ -0,0 +1,47 @@
+package com.itwillbs.goodbuy.vo;
+
+import java.sql.Date;
+import org.springframework.web.multipart.MultipartFile;
+import lombok.Data;
+
+@Data
+	public class MemberVO {
+		private int idx;
+		private String mem_name;
+		private String mem_id;
+		//private String mem_passwd1;
+		private String mem_passwd;
+		private String mem_nick;
+		private String mem_gender;
+		//--------------
+		private String mem_birthday;
+		//--------------
+		private String mem_phone;
+		private String mem_post_code;
+		private String mem_address1;
+		private String mem_address2;
+		//-----------------
+		private String mem_email;
+		private String mem_email1;
+		private String mem_email2;
+		//-----------------
+		private String mail_auth_status; // 이메일 인증상태 (Y:인증 N:미인증)
+		private String mem_grade;
+		//---------------------
+		private String terms1;
+		private String terms2;
+		private String terms3;
+
+		private int mem_status; //회원상태 (1:정상 2:대기 3:탈퇴)
+		private Date mem_reg_date;
+		private Date mem_withdraw_date;  
+		private String mem_like1_class;
+		private String mem_like2_class;
+		private MultipartFile mem_pp_file; //강사 포트폴리오 파일
+		private String file_pp; //강사 포트폴리오 이름
+		private String profile_image; //기본값 null
+		private String hashtag;
+		
+		
+
+}

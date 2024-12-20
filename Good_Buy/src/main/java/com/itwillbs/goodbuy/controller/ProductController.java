a6f6cbf9295e1011c7983a14bc120016eeb76035
LHM <paighkdlxld2@gmail.com>
2024-12-20 15:13:38 +0900

    햄

---
+++ Good_Buy/src/main/java/com/itwillbs/goodbuy/controller/ProductController.java
@@ -0,0 +1,25 @@
+package com.itwillbs.goodbuy.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+
+import com.itwillbs.goodbuy.service.ProductService;
+
+@Controller
+public class ProductController {
+	@Autowired ProductService productService;
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+}

4022703273cb2b6a1ec643b8ddca9e7071812a5b
BSH <qkdrjqnr05@naver.com>
2024-12-24 18:11:12 +0900

    FAQ 기본틀 & 채팅 약간 세팅

---
+++ Good_Buy/src/main/java/com/itwillbs/goodbuy/vo/FaqVO.java
@@ -0,0 +1,11 @@
+package com.itwillbs.goodbuy.vo;
+
+import lombok.Data;
+
+@Data
+public class FaqVO {
+	private int faq_id;
+	private String faq_subject;
+	private String faq_content;
+	private int faq_cate;
+}

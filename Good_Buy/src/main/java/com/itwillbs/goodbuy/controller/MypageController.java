83bae0907447da4d648529738533cb4ac827c17f
LHM <paighkdlxld2@gmail.com>
2024-12-30 14:49:28 +0900

    판매내역 필터링

--- Good_Buy/src/main/java/com/itwillbs/goodbuy/controller/MypageController.java
+++ Good_Buy/src/main/java/com/itwillbs/goodbuy/controller/MypageController.java
@@ -1,28 +1,21 @@
 package com.itwillbs.goodbuy.controller;
 
-import java.util.HashMap;
 import java.util.List;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpSession;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
-import com.itwillbs.goodbuy.aop.LoginCheck;
-import com.itwillbs.goodbuy.aop.LoginCheck.MemberRole;
 import com.itwillbs.goodbuy.service.MemberService;
 import com.itwillbs.goodbuy.service.MyPageService;
 import com.itwillbs.goodbuy.service.MyReviewService;
 import com.itwillbs.goodbuy.service.ProductService;
 import com.itwillbs.goodbuy.vo.MemberVO;
-import com.itwillbs.goodbuy.vo.MyPageVO;
 import com.itwillbs.goodbuy.vo.MyReviewVO;
 import com.itwillbs.goodbuy.vo.ProductVO;
 import com.itwillbs.goodbuy.vo.WishlistVO;

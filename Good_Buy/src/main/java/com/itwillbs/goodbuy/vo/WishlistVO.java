22044681c8beb8a22bcff9d171799f33e6ab5489
LHM <paighkdlxld2@gmail.com>
2025-01-06 19:45:50 +0900

    1:1문의 페이지 다시..살림..

--- Good_Buy/src/main/java/com/itwillbs/goodbuy/vo/WishlistVO.java
+++ Good_Buy/src/main/java/com/itwillbs/goodbuy/vo/WishlistVO.java
@@ -11,7 +11,6 @@ public class WishlistVO {
 	 private int wishlist_count;
 	 private int product_id; // 상품 ID
 	 private String product_title; // 상품 제목
-	 private String product_id; 	// 상품 번호
 	 private String mem_id;        // 회원 ID
 	 private String product_price;
 	 private String seller_nickname;

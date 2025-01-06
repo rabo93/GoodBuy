f59d7bc01fc42e736319c1492f29824d39187225
LHM <paighkdlxld2@gmail.com>
2025-01-06 19:25:28 +0900

    위시리스트 수정

--- Good_Buy/src/main/java/com/itwillbs/goodbuy/vo/WishlistVO.java
+++ Good_Buy/src/main/java/com/itwillbs/goodbuy/vo/WishlistVO.java
@@ -7,6 +7,13 @@ public class WishlistVO {
 
 	 private int wishlist_id;      // 위시리스트 ID
 	 private String product_title; // 상품 제목
+	 private String product_id; 	// 상품 번호
 	 private String mem_id;        // 회원 ID
+	 private String product_price;
+	 private String seller_nickname;
+	 private String product_category;
+	 private String product_trade_adr1;
+	 
+	 
 	
 }

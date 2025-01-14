c1a60461395f06f7efe15537a31c0812958f01b5
BSH <qkdrjqnr05@naver.com>
2025-01-15 08:21:50 +0900

    서버 변경

--- Good_Buy/src/main/webapp/resources/js/chat_header.js
+++ Good_Buy/src/main/webapp/resources/js/chat_header.js
@@ -39,8 +39,8 @@ $(function() {
 function connect() {
 //	let ws_base_url = "ws://itwillbs.com";
 //	let ws_base_url = "ws://localhost:8081";
-	let ws_base_url = "ws://localhost:8080";
-//	let ws_base_url = "ws://c3d2407t1p2.itwillbs.com/";
+//	let ws_base_url = "ws://localhost:8080";
+	let ws_base_url = "ws://c3d2407t1p2.itwillbs.com/";
 	ws = new WebSocket(ws_base_url + "/echo");
 	console.log("WebSocket 객체 : " + ws);
 	console.log("웹소켓 연결 상태 : " + ws.readyState);

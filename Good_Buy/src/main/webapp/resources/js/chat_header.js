7840272b8f06c04d7614e572069daacb05ae1c64
BSH <qkdrjqnr05@naver.com>
2025-01-17 13:53:27 +0900

    채팅 마무리

--- Good_Buy/src/main/webapp/resources/js/chat_header.js
+++ Good_Buy/src/main/webapp/resources/js/chat_header.js
@@ -44,11 +44,11 @@ $(function() {
 
 function connect() {
 //	let ws_base_url = "ws://itwillbs.com";
-//	let ws_base_url = "ws://localhost:8081";
+	let ws_base_url = "ws://localhost:8081";
 //	let ws_base_url = "ws://localhost:8080";
 //	let ws_base_url = "ws://c3d2407t1p2.itwillbs.com/";
 
-	let ws_base_url = window.location.href.replaceAll(window.location.href.split(':')[0], 'ws');	
+//	let ws_base_url = window.location.href.replaceAll(window.location.href.split(':')[0], 'ws');	
 
 	ws = new WebSocket(ws_base_url + "/echo");
 	console.log("WebSocket 객체 : " + ws);
@@ -95,15 +95,19 @@ function onMessage(event) {
 function chatProduct(e) {
 	let chatData = JSON.parse(e);
 	if(chatData.type == TYPE_START) {
-		console.log("room_id : " + chatData.room_id);
-		window.room_id = chatData.room_id;
-		console.log("window.room_id : " + window.room_id);
+		
+		$(".chat-container .chat-area").find(".chat-footer").append(`<input type="hidden" id="room_id" value="${chatData.room_id}">`);
+		
 		if($(".chat-body").children().length == 0) {
-			sendMessage(TYPE_TALK, "", sId, receiver_id, room_id, "안녕하세요 판매글 보고 연락드립니다.");
+			sendMessage(TYPE_TALK, "", sId, receiver_id, chatData.room_id, "안녕하세요 판매글 보고 연락드립니다.");
 		}
-	} else if (chatData.type == TYPE_TALK || chatData.type == TYPE_FILE) {
+		
+	} else {
 		appendMessage(chatData.type, chatData.sender_id, chatData.receiver_id, chatData.message, chatData.send_time);
 	}
+//	 if (chatData.type == TYPE_TALK || chatData.type == TYPE_FILE) {
+//		appendMessage(chatData.type, chatData.sender_id, chatData.receiver_id, chatData.message, chatData.send_time);
+//	}
 	
 }
 

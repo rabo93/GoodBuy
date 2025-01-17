96b723ca2730bb93746fcdaa2e498f2bf91f9f34
BSH <qkdrjqnr05@naver.com>
2025-01-17 19:01:57 +0900

    서버 주소

--- Good_Buy/src/main/webapp/resources/js/chat_header.js
+++ Good_Buy/src/main/webapp/resources/js/chat_header.js
@@ -44,9 +44,9 @@ $(function() {
 
 function connect() {
 //	let ws_base_url = "ws://itwillbs.com";
-	let ws_base_url = "ws://localhost:8081";
+//	let ws_base_url = "ws://localhost:8081";
 //	let ws_base_url = "ws://localhost:8080";
-//	let ws_base_url = "ws://c3d2407t1p2.itwillbs.com/";
+	let ws_base_url = "ws://c3d2407t1p2.itwillbs.com/";
 
 //	let ws_base_url = window.location.href.replaceAll(window.location.href.split(':')[0], 'ws');	
 
